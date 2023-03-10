a779eb5bb79b7fe02f898cdd14632e642f53aa0a
asg5450 <asg5450@gmail.com>
2023-03-10 12:01:26 +0900

    Spring없이 SOLID원칙 지키며 구현해보는 프로젝트
    Spring없이 SOLID원칙을 지키기 힘들다는 증명

---
+++ whyUseSpring/src/main/java/hello/whyusespring/WhyUseSpringApplication.java
@@ -0,0 +1,13 @@
+package hello.whyusespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyUseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhyUseSpringApplication.class, args);
+	}
+
+}
